034872c16a3884d74998c71ee95e4f6c02d5b722
LucasCoppola <lucascoppola21@gmail.com>
2024-06-09 12:47:04 -0300

    caching layer to explore locations

--- commands.go
+++ commands.go
@@ -19,8 +19,9 @@ type cliCommand struct {
 }
 
 type Config struct {
-	Next     string
-	Previous *string
+	Next             string
+	Previous         *string
+	VisitedLocations []string
 }
 
 type Location struct {
@@ -226,6 +227,39 @@ func commandExplore(args ...string) error {
 	name := args[0]
 	url := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%s/", name)
 
+	visited := false
+	for _, loc := range globalConfig.VisitedLocations {
+		if loc == url {
+			visited = true
+			break
+		}
+	}
+
+	// Cached Response
+	if visited {
+		val, ok := cache.Get(url)
+
+		if ok {
+			fmt.Println("--Explore Cached Response--")
+
+			var pokemonsFound SpecificLocationResponse
+			err := json.Unmarshal(val, &pokemonsFound)
+
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Pokemons found:")
+
+			for _, pokemonEnc := range pokemonsFound.PokemonEncounters {
+				fmt.Printf("- %s\n", pokemonEnc.Pokemon.Name)
+			}
+
+			return nil
+		}
+	}
+
+	// Non-Cached Response
 	res, err := http.Get(url)
 
 	if err != nil {
@@ -242,6 +276,10 @@ func commandExplore(args ...string) error {
 	if res.StatusCode > 299 {
 		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
 	}
+
+	// Add to Cache
+	cache.Add(url, body)
+	globalConfig.VisitedLocations = append(globalConfig.VisitedLocations, url)
 
 	var pokemonsFound SpecificLocationResponse
 	err = json.Unmarshal(body, &pokemonsFound)
